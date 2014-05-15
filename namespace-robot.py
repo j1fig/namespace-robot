fab2757608825ab579d33824492f57fd07384f78
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-05-15 15:08:24 -0400

    added argument parsing

--- namespace-robot.py
+++ namespace-robot.py
@@ -1,6 +1,13 @@
 from glob import glob
 from re import match
 from string import split
+import sys
+
+args = sys.argv[1:]
+
+for arg in args:
+    print arg
+
 
 print glob(".*")
 
@@ -14,6 +21,9 @@ for header_filename in glob("*.h"):
         namespace_match = match("namespace",line)
         if namespace_match:
             print line
+            line_words = split(line)
+            for word in line_words:
+                print word
 
 
 
