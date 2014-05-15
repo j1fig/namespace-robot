0740ce8ed5c2f3df88e33b4ef0325f41603daf6f
Joao Figueiredo <j1figuei@debian>
2014-05-15 14:58:26 -0400

    basic namespace detection implemented.

--- namespace-robot.py
+++ namespace-robot.py
@@ -1,11 +1,19 @@
-import glob
+from glob import glob
+from re import match
+from string import split
+
+print glob(".*")
 
 
-print glob.glob(".*")
-
-
-for header_filename in glob.glob("*.h"):
+for header_filename in glob("*.h"):
     print header_filename
     header_file = open(header_filename,'r+')
+
+    for line in header_file.readlines():
+        #print line
+        namespace_match = match("namespace",line)
+        if namespace_match:
+            print line
+
 
 
